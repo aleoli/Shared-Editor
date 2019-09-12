e786812130e34c541a872193d2e7b01a8351c530
Alessandro Olivero <aleoli96@gmail.com>
2019-09-12 19:38:07 +0200

    build fix on mac

--- cmake/qt_libraries.cmake
+++ cmake/qt_libraries.cmake
@@ -3,6 +3,13 @@ set(CMAKE_AUTOMOC ON)
 set(CMAKE_AUTOUIC ON)
 set(CMAKE_AUTORCC ON)
 
+if(APPLE AND EXISTS /usr/local/opt/qt5)
+	# Homebrew installs Qt5 (up to at least 5.9.1) in
+	# /usr/local/qt5, ensure it can be found by CMake since
+	# it is not in the default /usr/local prefix.
+	list(APPEND CMAKE_PREFIX_PATH "/usr/local/opt/qt5")
+endif()
+
 find_package(Qt5Core REQUIRED)
 find_package(Qt5Gui REQUIRED)
 find_package(Qt5Widgets REQUIRED)
