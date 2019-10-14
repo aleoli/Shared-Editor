a73f5151cd98fb4f2f0072a99abfdb0e486795b7
Gianluca Scopelliti <gianlu.1033@gmail.com>
2019-10-14 22:22:53 +0200

    update prova

--- src/client/test/prova_image.h
+++ src/client/test/prova_image.h
@@ -15,20 +15,25 @@ QByteArray saveImage(QImage &im) {
   QByteArray ba;
   QBuffer buffer(&ba);
   buffer.open(QIODevice::WriteOnly);
-  im.save(&buffer, "JPG");
-  return ba;
+  im.save(&buffer, "PNG");
+  return ba.toBase64();
 }
 
 void prova_image(QApplication &a) {
    QImage im1;
-   im1.load(":/other/malnati.jpg");
+   im1.load(":/other/malnati.png");
 
    auto ba = saveImage(im1);
 
-   QImage im2;
-   im2.loadFromData(ba, "JPG");
+   //std::cout << ba.toStdString() << std::endl;
 
-   showImage(im2);
+   QImage im2;
+   im2.loadFromData(QByteArray::fromBase64(ba), "PNG");
+
+   //show img
+   QLabel myLabel;
+   myLabel.setPixmap(QPixmap::fromImage(im2));
+   myLabel.show();
 
    std::cout << "Sono uguali: " << (im1 == im2) << std::endl;
 
