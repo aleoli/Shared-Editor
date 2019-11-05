76dbeb7872f15db7b701e2619b9aa1e67e9323cc
gianlu33 <36645341+gianlu33@users.noreply.github.com>
2019-11-05 15:21:46 +0100

    Processamento messaggi lato client (#35)
    * handlers
    
    * slot per comunicazioni client -> server
    
    * fix bug ClientMessageProcessor
    
    * add userId in USER::NEW and USER::LOGIN
    
    * fix dimenticanza di fileId
    
    * Aggiunto clientId nei messaggi REMOTE_XXX di FILE_EDIT

--- src/client/message_manager.cpp
+++ src/client/message_manager.cpp
@@ -3,6 +3,7 @@
 #include "exceptions.h"
 #include "Message.h"
 #include "client_message_processor.h"
+#include "utils.h"
 
 #include <iostream>
 
@@ -39,4 +40,249 @@ void MessageManager::process_data(QByteArray data) {
     std::cerr << ex.what() << std::endl;
     emit this->connection_error();
   }
+}
+
+// SLOT C->S
+
+void MessageManager::loginQuery(QString username, QString password) {
+  Message m{Message::Type::USER, static_cast<int>(Message::UserAction::LOGIN),
+    Message::Status::QUERY};
+
+    m.setValue("username", username);
+    m.setValue("password", password);
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::logoutQuery(QString token) {
+  Message m{Message::Type::USER, static_cast<int>(Message::UserAction::LOGOUT),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::newUserQuery(QString username, QString password, QString pswRepeat) {
+  Message m{Message::Type::USER, static_cast<int>(Message::UserAction::NEW),
+    Message::Status::QUERY};
+
+    m.setValue("username", username);
+    m.setValue("password", password);
+    m.setValue("pswRepeat", pswRepeat);
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::editUserQuery(QString token, QString nickname, QString oldPassword,
+  QString password, QString pswRepeat, QString icon) {
+    Message m{Message::Type::USER, static_cast<int>(Message::UserAction::EDIT),
+      Message::Status::QUERY};
+
+      m.setValue("token", token);
+
+      if(nickname != nullptr)
+        m.setValue("nickname", nickname);
+
+      if(oldPassword != nullptr && password != nullptr && pswRepeat != nullptr) {
+        m.setValue("oldPassword", oldPassword);
+        m.setValue("password", password);
+        m.setValue("pswRepeat", pswRepeat);
+      }
+
+      if(icon != nullptr)
+        m.setValue("icon", icon);
+
+      emit send_data(m.toQByteArray());
+}
+
+void MessageManager::deleteUserQuery(QString token) {
+  Message m{Message::Type::USER, static_cast<int>(Message::UserAction::DELETE),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::newFileQuery(QString token, QString name, int dirId) {
+  Message m{Message::Type::FILE, static_cast<int>(Message::FileAction::NEW),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+    m.setValue("name", name);
+
+    if(dirId != 0)
+      m.setValue("dirId", dirId);
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::getFileQuery(QString token, int fileId) {
+  Message m{Message::Type::FILE, static_cast<int>(Message::FileAction::GET),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+    m.setValue("fileId", fileId);
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::closeFileQuery(QString token, int fileId) {
+  Message m{Message::Type::FILE, static_cast<int>(Message::FileAction::CLOSE),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+    m.setValue("fileId", fileId);
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::editFileQuery(QString token, int fileId, QString name) {
+  Message m{Message::Type::FILE, static_cast<int>(Message::FileAction::EDIT),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+    m.setValue("fileId", fileId);
+
+    if(name != nullptr)
+      m.setValue("name", name);
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::deleteFileQuery(QString token, int fileId) {
+  Message m{Message::Type::FILE, static_cast<int>(Message::FileAction::DELETE),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+    m.setValue("fileId", fileId);
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::getLinkQuery(QString token, int fileId) {
+  Message m{Message::Type::FILE, static_cast<int>(Message::FileAction::GET_LINK),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+    m.setValue("fileId", fileId);
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::activateLinkQuery(QString token, QString link) {
+  Message m{Message::Type::FILE, static_cast<int>(Message::FileAction::ACTIVATE_LINK),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+    m.setValue("link", link);
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::localInsertQuery(QString token, int fileId, std::vector<Symbol> symbols) {
+  Message m{Message::Type::FILE_EDIT, static_cast<int>(Message::FileEditAction::LOCAL_INSERT),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+    m.setValue("fileId", fileId);
+    m.setValue("symbols", utils::vectorToJsonArray(symbols));
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::localDeleteQuery(QString token, int fileId, std::vector<SymbolId> ids) {
+  Message m{Message::Type::FILE_EDIT, static_cast<int>(Message::FileEditAction::LOCAL_DELETE),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+    m.setValue("fileId", fileId);
+    m.setValue("ids", utils::vectorToJsonArray(ids));
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::localUpdateQuery(QString token, int fileId, std::vector<Symbol> symbols) {
+  Message m{Message::Type::FILE_EDIT, static_cast<int>(Message::FileEditAction::LOCAL_UPDATE),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+    m.setValue("fileId", fileId);
+    //TODO aggiusta per non mandare il vettore pos dei symbols
+    m.setValue("symbols", utils::vectorToJsonArray(symbols));
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::localMoveQuery(QString token, int fileId, SymbolId symbolId, int cursorPosition) {
+  Message m{Message::Type::FILE_EDIT, static_cast<int>(Message::FileEditAction::LOCAL_MOVE),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+    m.setValue("fileId", fileId);
+    m.setValue("symbolId", symbolId.toJsonObject());
+    m.setValue("cursorPosition", cursorPosition);
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::newDirQuery(QString token, QString name, int parentId) {
+  Message m{Message::Type::FILESYSTEM, static_cast<int>(Message::FileSystemAction::NEW_DIR),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+    m.setValue("name", name);
+
+    if(parentId != 0)
+      m.setValue("parentId", parentId);
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::editDirQuery(QString token, int dirId, QString name, int parentId = -1) {
+  Message m{Message::Type::FILESYSTEM, static_cast<int>(Message::FileSystemAction::EDIT_DIR),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+    m.setValue("dirId", dirId);
+
+    if(name != nullptr)
+      m.setValue("name", name);
+
+    if(parentId != -1)
+      m.setValue("parentId", parentId);
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::deleteDirQuery(QString token, int dirId) {
+  Message m{Message::Type::FILESYSTEM, static_cast<int>(Message::FileSystemAction::DELETE_DIR),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+    m.setValue("dirId", dirId);
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::getDirQuery(QString token, int dirId) {
+  Message m{Message::Type::FILESYSTEM, static_cast<int>(Message::FileSystemAction::GET_DIR),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+    m.setValue("dirId", dirId);
+
+    emit send_data(m.toQByteArray());
+}
+
+void MessageManager::moveFileQuery(QString token, int fileId, int dirId) {
+  Message m{Message::Type::FILESYSTEM, static_cast<int>(Message::FileSystemAction::MOVE_FILE),
+    Message::Status::QUERY};
+
+    m.setValue("token", token);
+    m.setValue("fileId", fileId);
+    m.setValue("dirId", dirId);
+
+    emit send_data(m.toQByteArray());
 }
