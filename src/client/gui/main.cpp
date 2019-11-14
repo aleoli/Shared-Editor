a372f38153a58be078d5701f090e883df442c397
gianlu33 <36645341+gianlu33@users.noreply.github.com>
2019-11-14 17:43:46 +0100

    initial commit

--- src/client/gui/main.cpp
+++ src/client/gui/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
   SysConf conf = initiate_system(app);
   info("System successfully started");
 
-  //vero main
+  //vero main!
   //TODO vedere lo stub per come bisogna agire:
   // per collegare gli opportuni signal/slot serve una classe "Wrapper" che inglobi
   //tutte le componenti principali, sia grafiche che non
