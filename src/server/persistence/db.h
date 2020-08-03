91abb9654d56ce59ad0eac4eda1a8be49957a8d6
Alessandro Olivero <aleoli96@gmail.com>
2020-08-03 22:06:54 +0200

    db

--- src/server/persistence/db.h
+++ src/server/persistence/db.h
@@ -23,101 +23,101 @@ using namespace se_exceptions;
 
 class persistence::DB {
 public:
-    DB(const DB&) = delete;
-    DB(DB&&) = delete;
-    DB& operator=(const DB&) = delete;
-    DB& operator=(DB&&) = delete;
+  DB(const DB&) = delete;
+  DB(DB&&) = delete;
+  DB& operator=(const DB&) = delete;
+  DB& operator=(DB&&) = delete;
 
-    static std::shared_ptr<DB> get(bool mem_only = false) {
-        if(DB::instance == nullptr) {
-            DB::instance.reset(new DB{mem_only});
-        }
-        return DB::instance;
+  static std::shared_ptr<DB> get(bool mem_only = false) {
+    if(DB::instance == nullptr) {
+      DB::instance.reset(new DB{mem_only});
     }
+    return DB::instance;
+  }
 
-    static void create_db_if_not_exists(const QString& path = "") {
-      QSqlQuery query;
-      QStringList strs;
-      if(path != "") {
-        QFile f(path);
-        if(!f.open(QIODevice::ReadOnly)) {
-          std::cerr << "ERRORE: non trovo il file per la creazione del database" << std::endl;
-          exit(1);
-        }
-        strs = QTextStream(&f).readAll().split(";");
-      } else {
-        strs = DB::get()->create_db;
+  static void create_db_if_not_exists(const QString& path = "") {
+    QSqlQuery query;
+    QStringList strs;
+    if(path != "") {
+      QFile f(path);
+      if(!f.open(QIODevice::ReadOnly)) {
+        std::cerr << "ERRORE: non trovo il file per la creazione del database" << std::endl;
+        exit(1);
       }
+      strs = QTextStream(&f).readAll().split(";");
+    } else {
+      strs = DB::get()->create_db;
+    }
 
-      for(QString& str: strs) {
-        if(str.trimmed().isEmpty()) {
-          continue;
-        }
-        if(!query.exec(str)) {
-          std::cerr << "ERRORE: " << query.lastError().text().toStdString() << std::endl;
-          exit(1);
-        }
-        query.finish();
+    for(QString& str: strs) {
+      if(str.trimmed().isEmpty()) {
+        continue;
       }
+      if(!query.exec(str)) {
+        std::cerr << "ERRORE: " << query.lastError().text().toStdString() << std::endl;
+        exit(1);
+      }
+      query.finish();
     }
+  }
 
-    template<typename T> static std::vector<T> getAll() {
-        std::vector<T> v;
-        QSqlTableModel model;
-        model.setTable(T::table_name);
-        model.select();
-        for(int i=0; i<model.rowCount(); ++i) {
-            v.push_back(T{model.record(i)});
-        }
-        return v;
+  template<typename T> static std::vector<T> getAll() {
+    std::vector<T> v;
+    QSqlTableModel model;
+    model.setTable(T::table_name);
+    model.select();
+    for(int i=0; i<model.rowCount(); ++i) {
+      v.push_back(T{model.record(i)});
     }
+    return v;
+  }
 
-    template<typename T> static T getOne(int id) {
-        QSqlTableModel model;
-        model.setTable(T::table_name);
-        model.setFilter("id = "+QString::number(id));
-        model.select();
-        if(model.rowCount() > 0) {
-            return T{model.record(0)};
-        } else {
-            throw SQLNoElementSelectException{"No element found in table '"+T::table_name+"' for id "+QString::number(id)};
-        }
+  template<typename T> static T getOne(int id) {
+    QSqlTableModel model;
+    model.setTable(T::table_name);
+    model.setFilter("id = "+QString::number(id));
+    model.select();
+    if(model.rowCount() > 0) {
+      return T{model.record(0)};
+    } else {
+      throw SQLNoElementSelectException{"No element found in table '"+T::table_name+"' for id "+QString::number(id)};
     }
+  }
 
-    template<typename T> static std::vector<T> get(const QString& filter) {
-        std::vector<T> v;
-        QSqlTableModel model;
-        model.setTable(T::table_name);
-        model.setFilter(filter);
-        model.select();
-        for(int i=0; i<model.rowCount(); ++i) {
-            v.push_back(T{model.record(i)});
-        }
-        return v;
+  template<typename T> static std::vector<T> get(const QString& filter) {
+    std::vector<T> v;
+    QSqlTableModel model;
+    model.setTable(T::table_name);
+    model.setFilter(filter);
+    model.select();
+    for(int i=0; i<model.rowCount(); ++i) {
+      v.push_back(T{model.record(i)});
     }
+    return v;
+  }
 
 private:
     static std::shared_ptr<DB> instance;
 
     explicit DB(bool mem_only = false) {
-        if(!QSqlDatabase::isDriverAvailable("QSQLITE")) {
-          std::cerr << "ERRORE: No Driver" << std::endl;
-          exit(1);    // se non ha i driver del DB posso anche interrompere il programma, tanto non può funzionare
-        }
-        db = QSqlDatabase::addDatabase("QSQLITE");
-        if(mem_only) {
-          debug("mem-only database");
-          db.setDatabaseName(":memory:");
-        } else {
-          db.setDatabaseName(QDir::homePath()+"/.shared_editor/shared-editor.db");
-        }
+      if(!QSqlDatabase::isDriverAvailable("QSQLITE")) {
+        std::cerr << "ERRORE: No Driver" << std::endl;
+        exit(1);    // se non ha i driver del DB posso anche interrompere il programma, tanto non può funzionare
+      }
+      db = QSqlDatabase::addDatabase("QSQLITE");
+      if(mem_only) {
+        debug("mem-only database");
+        db.setDatabaseName(":memory:");
+      } else {
+        db.setDatabaseName(QDir::homePath()+"/.shared_editor/shared-editor.db");
+      }
 
-        if(!db.open()) {
-            std::cerr << "ERRORE: connessione al DB fallita" << std::endl << db.lastError().text().toStdString() << std::endl;
-            exit(1);  // idem come sopra
-        }
+      if(!db.open()) {
+          std::cerr << "ERRORE: connessione al DB fallita" << std::endl << db.lastError().text().toStdString() << std::endl;
+          exit(1);  // idem come sopra
+      }
 
-        debug("DB connesso");
+      debug("DB connesso");
     }
 
     QSqlDatabase db;
