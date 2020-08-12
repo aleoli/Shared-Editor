f74e8899f2266f508813c1b6f3acad20974c0e77
Gianluca Scopelliti <gianlu.1033@gmail.com>
2020-08-12 18:05:59 +0200

    remove updates on start_server.bat

--- windows/start_server.bat
+++ windows/start_server.bat
@@ -2,6 +2,6 @@
 
 set /p loglevel=Enter log level (none - error - warn - info - debug):
 
-..\bin\server.exe -l %loglevel% -m
+..\bin\server.exe -l %loglevel%
 
 set /p=Hit ENTER to close the window..
