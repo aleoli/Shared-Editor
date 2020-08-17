db4e9b28fa4ac31ab012656f360ca90e5e2adf3a
Gianluca Scopelliti <gianlu.1033@gmail.com>
2020-08-17 22:01:41 +0200

    messagemanager: const references everywhere

--- src/client/message_manager.h
+++ src/client/message_manager.h
@@ -21,76 +21,75 @@ public:
   ~MessageManager();
 
 signals:
-  void send_data(QByteArray data);
+  void send_data(const QByteArray &data);
   void connection_error();
 
   // messaggi Server -> Client. La GUI deve collegarci gli slot
-  void errorResponse(QString reason);
+  void errorResponse(const QString &reason);
 
-  void loginResponse(QString token, int userId, std::optional<QString> nickname, std::optional<QString> icon);
-  void newUserResponse(QString token, int userId);
+  void loginResponse(const QString &token, int userId, const std::optional<QString> &nickname, const std::optional<QString> &icon);
+  void newUserResponse(const QString &token, int userId);
   void editUserResponse();
   void deleteUserResponse();
   void newFileResponse(int fileId);
-  void getFileResponse(File file, int charId);
+  void getFileResponse(const File &file, int charId);
   void editFileResponse();
   void deleteFileResponse();
   void fileDeletedQuery(int fileId);
-  void getLinkResponse(QString link);
-  void activateLinkResponse(FSElement element, File file);
+  void getLinkResponse(const QString &link);
+  void activateLinkResponse(const FSElement &element, const File &file);
 
-  void remoteInsertQuery(int fileId, int userId, std::vector<Symbol> symbols);
-  void remoteDeleteQuery(int fileId, int userId, std::vector<SymbolId> ids);
-  void remoteUpdateQuery(int fileId, int userId, std::vector<Symbol> symbols);
-  void userConnectedQuery(int fileId, int userId, QString username);
+  void remoteInsertQuery(int fileId, int userId, const std::vector<Symbol> &symbols);
+  void remoteDeleteQuery(int fileId, int userId, const std::vector<SymbolId> &ids);
+  void remoteUpdateQuery(int fileId, int userId, const std::vector<Symbol> &symbols);
+  void userConnectedQuery(int fileId, int userId, const QString &username);
   void userDisconnectedQuery(int fileId, int userId);
-  void remoteMoveQuery(int fileId, int userId, SymbolId symbolId, int cursorPosition);
+  void remoteMoveQuery(int fileId, int userId, const SymbolId &symbolId, int cursorPosition);
 
   void newDirResponse(int dirId);
   void editDirResponse();
   void deleteDirResponse();
-  void getDirResponse(std::vector<FSElement> elements);
+  void getDirResponse(const std::vector<FSElement> &elements);
   void moveFileResponse();
 
-  void commentRemoteInsertQuery(int fileId, int userId, File::Comment comment);
-  void commentRemoteUpdateQuery(int fileId, int userId, File::Comment comment);
-  void commentRemoteDeleteQuery(int fileId, int userId, File::Comment comment);
+  void commentRemoteInsertQuery(int fileId, int userId, const File::Comment &comment);
+  void commentRemoteUpdateQuery(int fileId, int userId, const File::Comment &comment);
+  void commentRemoteDeleteQuery(int fileId, int userId, const File::Comment &comment);
 
 public slots:
-  void process_data(QByteArray data);
+  void process_data(const QByteArray &data);
 
   // Messaggi Client -> Server. La GUI deve collegarci dei segnali
-  void loginQuery(QString username, QString password);
-  void logoutQuery(QString token);
-  void newUserQuery(QString username, QString password, QString pswRepeat);
-  void editUserQuery(QString token, std::optional<QString> nickname, std::optional<QString> oldPassword,
-    std::optional<QString> password, std::optional<QString> pswRepeat, std::optional<QString> icon);
-  void deleteUserQuery(QString token);
+  void loginQuery(const QString &username, const QString &password);
+  void logoutQuery(const QString &token);
+  void newUserQuery(const QString &username, const QString &password, const QString &pswRepeat);
+  void editUserQuery(const QString &token, const std::optional<QString> &nickname, const std::optional<QString> &oldPassword, const std::optional<QString> &password, const std::optional<QString> &pswRepeat, const std::optional<QString> &icon);
+  void deleteUserQuery(const QString &token);
 
   // newFileQuery: se il file è creato nella root, il dirId non è necessario
-  void newFileQuery(QString token, QString name, std::optional<int> dirId = std::nullopt);
-  void getFileQuery(QString token, int fileId);
-  void closeFileQuery(QString token, int fileId);
-  void editFileQuery(QString token, int fileId, std::optional<QString> name);
-  void deleteFileQuery(QString token, int fileId);
-  void getLinkQuery(QString token, int fileId);
-  void activateLinkQuery(QString token, QString link);
+  void newFileQuery(const QString &token, const QString &name, const std::optional<int> &dirId = std::nullopt);
+  void getFileQuery(const QString &token, int fileId);
+  void closeFileQuery(const QString &token, int fileId);
+  void editFileQuery(const QString &token, int fileId, const std::optional<QString> &name);
+  void deleteFileQuery(const QString &token, int fileId);
+  void getLinkQuery(const QString &token, int fileId);
+  void activateLinkQuery(const QString &token, const QString &link);
 
-  void localInsertQuery(QString token, int fileId, std::vector<Symbol> symbols);
-  void localDeleteQuery(QString token, int fileId, std::vector<SymbolId> ids);
-  void localUpdateQuery(QString token, int fileId, std::vector<Symbol> symbols);
-  void localMoveQuery(QString token, int fileId, SymbolId symbolId, int cursorPosition);
+  void localInsertQuery(const QString &token, int fileId, const std::vector<Symbol> &symbols);
+  void localDeleteQuery(const QString &token, int fileId, const std::vector<SymbolId> &ids);
+  void localUpdateQuery(const QString &token, int fileId, const std::vector<Symbol> &symbols);
+  void localMoveQuery(const QString &token, int fileId, const SymbolId &symbolId, int cursorPosition);
 
   // newDirQuery: se la dir è creata nella root, il parentId non è necessario
-  void newDirQuery(QString token, QString name, std::optional<int> parentId = std::nullopt);
-  void editDirQuery(QString token, int dirId, std::optional<QString> name, std::optional<int> parentId);
-  void deleteDirQuery(QString token, int dirId);
-  void getDirQuery(QString token, std::optional<int> dirId = std::nullopt);
-  void moveFileQuery(QString token, int fileId, int dirId);
+  void newDirQuery(const QString &token, const QString &name, const std::optional<int> &parentId = std::nullopt);
+  void editDirQuery(const QString &token, int dirId, const std::optional<QString> &name, const std::optional<int> &parentId);
+  void deleteDirQuery(const QString &token, int dirId);
+  void getDirQuery(const QString &token, const std::optional<int> &dirId = std::nullopt);
+  void moveFileQuery(const QString &token, int fileId, int dirId);
 
-  void commentLocalInsertQuery(QString token, int fileId, File::Comment comment);
-  void commentLocalUpdateQuery(QString token, int fileId, File::Comment comment);
-  void commentLocalDeleteQuery(QString token, int fileId, File::Comment comment);
+  void commentLocalInsertQuery(const QString &token, int fileId, const File::Comment &comment);
+  void commentLocalUpdateQuery(const QString &token, int fileId, const File::Comment &comment);
+  void commentLocalDeleteQuery(const QString &token, int fileId, const File::Comment &comment);
 
 private:
   static std::shared_ptr<MessageManager> instance;
