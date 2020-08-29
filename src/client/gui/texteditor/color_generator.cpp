f0334e0dc453e9365ce6e80b49f79d0359cfbea4
Gianluca Scopelliti <gianlu.1033@gmail.com>
2020-08-29 11:34:31 +0200

    improve ColorGenerator logic

--- src/client/gui/texteditor/color_generator.cpp
+++ src/client/gui/texteditor/color_generator.cpp
@@ -1,26 +1,50 @@
 #include "color_generator.h"
 
 #include <QString>
+#include <QRandomGenerator>
+
+#include <algorithm>
 
 #include "exceptions.h"
 #include "utils.h"
 
+#define NUMBER_TRIES 5
+#define HUE_DIFFERENCE 15
+
 using namespace se_exceptions;
 
-ColorGenerator::ColorGenerator() : _maxColors(256) {}
-
-ColorGenerator::ColorGenerator(int maxColors) : _maxColors(maxColors) {}
+ColorGenerator::ColorGenerator() {}
 
 QColor ColorGenerator::getColor() {
-  if(_counter >= _colors.size()) {
-    throw GuiException{"Out of colors!"};
+  for(int i=0; i<NUMBER_TRIES; i++) {
+    auto hue = QRandomGenerator::global()->bounded(0, 360) / HUE_DIFFERENCE;
+    auto sat = QRandomGenerator::global()->bounded(0, 3);
+    auto light = QRandomGenerator::global()->bounded(0, 3);
+    //debug("Hue: " + QString::number(hue) + " Sat: " + QString::number(sat) + " Light: " + QString::number(light));
+
+    // Hue: 0-359 Sat: 0-255 Light: 0-255
+    // Number of combinations: 360/HUE_DIFFERENCE * 3 * 3
+    // If HUE_DIFFERENCE == 15 => 24*3*3 = 216 possible colors
+
+    auto tuple = std::make_tuple(hue, sat, light);
+    auto it = std::find(_colors.begin(), _colors.end(), tuple);
+
+    if (it == _colors.end()) {
+      //color not yet used
+      _colors.push_back(tuple);
+      return QColor::fromHsl(hue * HUE_DIFFERENCE, _saturation[sat], _lightness[light]);
+    }
+
+    if(i == NUMBER_TRIES-1) {
+      // I give up, I reuse the same color..
+      warn("Using the same color previously used..");
+      return QColor::fromHsl(hue * HUE_DIFFERENCE, _saturation[sat], _lightness[light]);
+    }
   }
 
-  auto color = _colors.at(_counter++);
-
-  return QColor(color);
+  throw GuiException{"Should never reach this instruction"};
 }
 
 void ColorGenerator::reset() {
-  _counter = 0;
+  _colors.clear();
 }
